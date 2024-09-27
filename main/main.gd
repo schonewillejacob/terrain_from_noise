eff7c9b7bf389d7bbd079198e5e90bd043534564
Jacob <winkschonewille@gmail.com>
2024-09-27 11:42:14 -0700

    Grid si aligns and printing red channel as new albedo.

--- main/main.gd
+++ main/main.gd
@@ -17,15 +17,15 @@ var height_map : Image
 
 # VIRTUALS ####################################################
 func _ready() -> void:
-	# testing purposes: create red gradient
-	height_map = Image.create_empty(noise_texture_side_length,noise_texture_side_length, false, Image.FORMAT_RG8) # all white image for testing purposes
-	for _x in noise_texture_side_length:
-		for _y in noise_texture_side_length:
-			var _target : float = float(_x) / float(noise_texture_side_length)
-			height_map.set_pixel(_x,_y,Color(_target,0,0,1))
+	## testing purposes: create red gradient
+	#height_map = Image.create_empty(noise_texture_side_length,noise_texture_side_length, false, Image.FORMAT_RG8) # all white image for testing purposes
+	#for _x in noise_texture_side_length:
+		#for _y in noise_texture_side_length:
+			#var _target : float = float(_x) / float(noise_texture_side_length)
+			#height_map.set_pixel(_x,_y,Color(_target,0,0,1))
 	
 	# generate noise UV
-	#height_map = create_height_map()
+	height_map = create_height_map()
 	
 	# use a two-row approach to set vertex heights.
 	#	the length of each row should be the grid side-length + 1
@@ -38,7 +38,7 @@ func _ready() -> void:
 	for _z in quadmesh_grid_side_length:
 		if(_z % 2 == 0):
 			_vertex_row_odd = set_row_heights_from_heightmap(_z+1)
-			add_landscape_quad_row(_vertex_row_even, _vertex_row_odd, _z)
+			add_landscape_quad_row(_vertex_row_odd, _vertex_row_even, _z)
 		else:
 			_vertex_row_even = set_row_heights_from_heightmap(_z+1)
 			add_landscape_quad_row(_vertex_row_even, _vertex_row_odd, _z)
@@ -53,8 +53,8 @@ func _ready() -> void:
 func create_height_map() -> Image:
 	# goal: long, thin cells.
 	var _noise = FastNoiseLite.new()
-	#randomize()
-	#_noise.seed = randf() * 10_000_000 # set seed to a random integer from 0 - 9,999,999
+	randomize()
+	_noise.seed = randf() * 10_000_000 # set seed to a random integer from 0 - 9,999,999
 	
 	# NOISE CONFIGS
 	_noise.cellular_distance_function = 2
@@ -64,31 +64,31 @@ func create_height_map() -> Image:
 	_noise.fractal_lacunarity = 1
 	_noise.fractal_gain = .005
 	
-	_noise.fractal_ping_pong_strength = 16
+	_noise.fractal_ping_pong_strength = 2
 	# /NOISE CONFIGS
 	
 	var _noise_image = _noise.get_image(noise_texture_side_length,noise_texture_side_length) # no point in this being seamless, as the heightmap won't be tiled.
 	
 	return _noise_image
 
-func add_landscape_quad_row(top_row,bottom_row,z_grid_position) -> void:
+func add_landscape_quad_row(top_row : Array, bottom_row : Array, z_grid_position : int) -> void:
 	for _x in quadmesh_grid_side_length:
 		var _land = MeshInstance3D.new()
 		var _surface_tool = SurfaceTool.new()
 		
 		_surface_tool.begin(Mesh.PRIMITIVE_TRIANGLES)
-		# vertex 0
+		# vertex 0,0 = bottom-left
 		_surface_tool.set_uv( Vector2(0, 0) )
-		_surface_tool.add_vertex( Vector3(0, 0, 0) ) 
-		# vertex 1
+		_surface_tool.add_vertex( Vector3(0, bottom_row[_x], 0) ) 
+		# vertex 1,0 = bottom-right
 		_surface_tool.set_uv( Vector2(1, 0) )
-		_surface_tool.add_vertex( Vector3(1, 0, 0) ) 
-		# vertex 2
+		_surface_tool.add_vertex( Vector3(1, bottom_row[_x+1], 0) ) 
+		# vertex 1,1 = top-right
 		_surface_tool.set_uv( Vector2(1, 1) )
-		_surface_tool.add_vertex( Vector3(1, 0, 1) ) 
-		# vertex 3
+		_surface_tool.add_vertex( Vector3(1, top_row[_x+1], 1) ) 
+		# vertex 0,1 = top-left
 		_surface_tool.set_uv( Vector2(0, 1) )
-		_surface_tool.add_vertex( Vector3(0, 0, 1) ) 
+		_surface_tool.add_vertex( Vector3(0, top_row[_x], 1) ) 
 		# make the fir_surface_tool triangle
 		_surface_tool.add_index(0) 
 		_surface_tool.add_index(1)
@@ -101,13 +101,20 @@ func add_landscape_quad_row(top_row,bottom_row,z_grid_position) -> void:
 		_surface_tool.generate_normals() # normals point perpendicular up from each face
 		var _mesh = _surface_tool.commit() # arranges mesh data structures into arrays for us
 		
-		_land.mesh = _mesh 
+		_land.mesh = _mesh
+		var _height_colour_material = StandardMaterial3D.new()
+		_height_colour_material.albedo_color = Color(top_row[_x],0,0,1)
+		
+		_land.mesh.surface_set_material(0,_height_colour_material)
+		_land.position = Vector3(_x,0,z_grid_position)
+		
+		node_landscape.add_child(_land)
 	
-func set_row_heights_from_heightmap(pos_z) -> Array:
+func set_row_heights_from_heightmap(pos_z : int) -> Array:
 	var _row_heights : Array
-	for _position in (quadmesh_grid_side_length + 1):
-		var _x_target = float(_position)/float(quadmesh_grid_side_length) * float(noise_texture_side_length)
+	for _x_position in (quadmesh_grid_side_length + 1):
+		var _x_target = float(_x_position)/float(quadmesh_grid_side_length) * float(noise_texture_side_length)
+		_x_target = clamp(_x_target-1,0,noise_texture_side_length-1)
 		_row_heights.append(height_map.get_pixel(_x_target, pos_z).r) # returns a float of the pixel's red channel, from 0-1
 		
-	print(_row_heights)
 	return _row_heights
